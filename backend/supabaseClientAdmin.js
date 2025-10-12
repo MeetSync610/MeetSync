30e7c53295b0d3cd27da6836d32f70850fddb14a
Sancol610 <santycolthompson@gmail.com>
2025-10-11 22:52:39 -0300

    envir

--- backend/supabaseClientAdmin.js
+++ backend/supabaseClientAdmin.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // ⚠️ importante
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseAdmin = createClient(
-  process.env.VITE_SUPABASE_URL,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
